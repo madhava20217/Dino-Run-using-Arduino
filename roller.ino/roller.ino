1b042e8e875ae9cd7eca9e6c8c59410e7e5cb0d3
Madhava <madhava20217@iiitd.ac.in>
2022-05-11 00:08:38 +0530

    testing IR

--- roller.ino/roller.ino
+++ roller.ino/roller.ino
@@ -1,14 +1,53 @@
 #include <Arduino.h>
-#include <Stepper.h>
+#include <Servo.h>
+#include <IRremote.h>
 
 
-Stepper
+Servo motor;
 
-int pos = 0;
+int serv_pin = 8;
+
+int recv_pin = 2;
+
+IRrecv irrecv(recv_pin);
+
 void setup(){
-    myservo.attach(8);
+
+
+    motor.attach(serv_pin);
+    Serial.begin(9600);
+    irrecv.enableIRIn();
+
+    Serial.println("HELLO!");
 }
 
+decode_results results;
+
 void loop(){
+
+    if(irrecv.decode(&results)){
+        //int results.value = results;
+        Serial.println(" ");
+        Serial.print("CODE: ");
+        Serial.println(results.value, HEX);
+
+
+        delay(1500);
+        irrecv.resume();
+    }
     
+    // if(!in) {
+    //     digitalWrite(13, HIGH);
+    //     delay(1000);
+    //     digitalWrite(13, LOW);
+    // }
+    
+    //if(!in) servo_protocol();
+}
+
+void servo_protocol(){
+    for(int i = 0; i <= 180; i++){
+        motor.write(i);
+        delay(2);
+    }
 }
