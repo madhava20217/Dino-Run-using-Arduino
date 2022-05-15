09af38f24dc3efcc77d9b5faedbb270b0b09dc4e
= <madhava20217@iiitd.ac.in>
2022-05-15 18:57:45 +0530

    conveyor belt

--- conveyor_belt/conveyor_belt.ino
+++ conveyor_belt/conveyor_belt.ino
@@ -1,15 +1,18 @@
 #include <Servo.h>
 
 int pin_in = 7;
+int pin_in2 = 6;
 
-Servo conveyor;
+Servo conveyor1;
+Servo conveyor2;
 
 void setup(){
-    conveyor.attach(pin_in);
+    conveyor1.attach(pin_in);
+    conveyor2.attach(pin_in2);
 }
 
 void loop(){
-    conveyor.write(0);
-    delay(5000);
-    conveyor.detach();
+    conveyor1.write(0);
+    conveyor2.write(0);
+
 }
