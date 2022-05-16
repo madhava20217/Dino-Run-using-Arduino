4e5f4e7fc5cddf4464699e895012f8d3661358a6
= <madhava20217@iiitd.ac.in>
2022-05-16 17:25:24 +0530

    knight movement works

--- knight/knight.ino
+++ knight/knight.ino
@@ -4,7 +4,9 @@
 #define START_ANGLE 115
 #define END_ANGLE 160
 
-int threshold = 30;         //creates an alarm if greater than the value specified
+const int JUMP_TIME = 1000;
+
+int threshold = 10000;
 
 int shck_pin = 5;           // pin for shock sensor
 int movement = 7;           // pin for movement
@@ -19,24 +21,38 @@ void setup(){
     movt.attach(movement);
 
     pinMode(button, INPUT);
+    movt.write(START_ANGLE);
 }
 
-
+int start;      // for storing time of jumping
+bool pressed = false;
 
 void loop(){
 
-    // if(analogRead(shck_pin) > threshold){
-    //     //Do something
-    //     Serial.print("ALARM ");
-    //     Serial.println(num++);
-    //     delay(500);
-    // }
-    pulseIn(shck_pin, HIGH);
+    // int pulse = pulseIn(shck_pin, HIGH);
     
+    // Serial.println(pulse);
+
+    int in = digitalRead(button);
+    if(in == HIGH){
+        //Serial.println("YEET");
+        if(start == 0){
+            // jump
+            jump_down();
+            start = millis();
+        }
+    }
+    if(start != 0 && millis() - start > JUMP_TIME){
+            start = 0;
+            jump_up();
+        }
+
+}
+
+void jump_up(){
     movt.write(START_ANGLE);
-    Serial.println(START_ANGLE);
-    delay(5000);
+}
+
+void jump_down(){
     movt.write(END_ANGLE);
-    Serial.println("END ANGLE");
-    delay(5000);
 }
