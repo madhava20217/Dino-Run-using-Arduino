867efdc4c42cca0860a27b7e6a8204ad0390f0bd
= <madhava20217@iiitd.ac.in>
2022-05-16 20:01:05 +0530

    DONE

--- knight/knight.ino
+++ knight/knight.ino
@@ -7,7 +7,7 @@
 const int JUMP_TIME = 1000;
 const int cooldown_time = 1000;
 
-int threshold = 10000;
+int threshold = 20000;
 
 int shck_pin = 5;           // pin for shock sensor
 int movement = 7;           // pin for movement
@@ -25,18 +25,25 @@ void setup(){
     movt.write(START_ANGLE);
 }
 
+long beginning = millis();       //start of the game
+
 int start;      // for storing time of jumping
 bool cooldown = false;
 
+int i = 1;
+
 void loop(){
 
-    // int pulse = pulseIn(shck_pin, HIGH);
-    
-    // Serial.println(pulse);
+    int pulse = pulseIn(shck_pin, HIGH);
+    if(pulse >= threshold)
+        {
+            gameover();
+
+            //exit(0);
+        }
 
     int in = digitalRead(button);
     if(in == HIGH){
-        //Serial.println("YEET");
         if(start == 0 && !cooldown){
             // jump
             jump_down();
@@ -44,9 +51,15 @@ void loop(){
         }
     }
     if(start != 0 && millis() - start > JUMP_TIME){
+        // pulse = pulseIn(shck_pin, HIGH);
+        // Serial.println("Jump pulse: ");
+        // Serial.println(pulse);
         jump_up();
     }
     if(start != 0 && millis()-start > JUMP_TIME+cooldown_time){
+        // pulse = pulseIn(shck_pin, HIGH);
+        // Serial.println("Cooldown pulse: ");
+        // Serial.println(pulse);
         start = 0;
     }
 
@@ -58,4 +71,14 @@ void jump_up(){
 
 void jump_down(){
     movt.write(END_ANGLE);
+}
+
+void gameover(){
+    Serial.println("GAME OVER");
+    Serial.print("You were alive for ");
+    Serial.print((millis() - beginning)/1000);
+    Serial.println(" seconds");
+    delay(1000);
+
+    beginning = millis();
 }
